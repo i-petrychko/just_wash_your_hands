b85d76dc29ac10fba0f347d1c276067c75b85f7a
vitalii <vitalii.petrychko@leven.vision>
2025-03-20 14:52:07 +0200

    fixed statup script

--- aws/startup.sh
+++ aws/startup.sh
@@ -9,7 +9,7 @@ sudo tar xzf ./actions-runner-linux-x64-2.317.0.tar.gz;
 echo Registering the runner with token $TOKEN;
 export RUNNER_ALLOW_RUNASROOT=1;
 sudo yum install -y dotnet-sdk-6.0
-sudo -E ./config.sh --url https://github.com/Leven-Vision/svs-processing --token $TOKEN --unattended --ephemeral --labels training-runner;
+sudo -E ./config.sh --url https://github.com/i-petrychko/just_wash_your_hands --token $TOKEN --unattended --ephemeral --labels training-runner;
 
 echo Starting the runner...;
 sudo -E ./run.sh;
